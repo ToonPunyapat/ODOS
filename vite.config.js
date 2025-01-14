35b873822b5f40d0f0305fd1fd0c2047cc5e77b8
fano89 <6460506021@pnu.ac.th>
2025-01-14 17:01:44 +0700

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import removeConsole from 'vite-plugin-remove-console';
+import obfuscator from 'vite-plugin-javascript-obfuscator';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    removeConsole(),
+    obfuscator({
+      globalOptions: {
+        debugProtection: true,
+        disableConsoleOutput: true,
+      },
+    }),
+  ],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      mangle: {
+        properties: {
+          regex: /^_/,
+        },
+      },
+    },
+  },
+});
